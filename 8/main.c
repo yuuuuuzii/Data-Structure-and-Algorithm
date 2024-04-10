14e1a381626cc72aa54ad7868ceab1cefc9dad2c
林祐丞 <b08501095@ntu.edu.tw>
2024-04-10 15:10:28 +0800

    test

---
+++ 8/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node *next;
+}Node;
+
+void FCD(Node *head){
+    Node *hare = head;
+    Node *tortoise = head;
+    while(hare != NULL && hare->next != NULL){
+        printf("%d ",hare->data);
+        hare = (hare->next)->next;
+        tortoise = tortoise->next;
+        if (hare == tortoise){
+            printf("%d ",hare->data);
+            break; 
+        }
+    }
+}
+int main(){
+    int num;
+    scanf("%d",&num);
+    Node *head = NULL;
+    Node *temp = NULL;
+    int data = 0;
+    int next = 0;
+    for(int i = 1;i<=num;i++){
+        Node *new = (Node *)malloc(sizeof(Node));
+        new->next = NULL;
+        scanf("%d",&data);
+        new->data = data;
+        if(i == 1){
+            head = new;
+            temp = new;
+        }
+        if(next == i){
+            temp->next = new;
+            temp = temp->next;
+        }
+        scanf("%d",&next);
+        if(next != 0 && next != i+1){
+            Node *run  = head;
+            for(int i = 0;i<(next-1);i++){
+                run = run->next;
+            }
+            new->next = run;
+        }
+    }
+    FCD(head);
+}
