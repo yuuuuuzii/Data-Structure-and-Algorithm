4d79d1558bbfbab7a96a649800e26d12c6569feb
林祐丞 <b08501095@ntu.edu.tw>
2024-05-06 14:59:13 +0800

    P

--- 16/main.c
+++ 16/main.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-long long *Prefix(char* str,long long *arr);
+long long *Prefix(char* str, long long *arr, long long size);
 
-long long *KMP(char *source, char *copy, long long *arr){
+long long *KMP(char *source, char *copy, long long *arr,long long m,long long *pla){
     long long n = strlen(source);
-    long long m = strlen(copy);
     
     long long k = -1;
     long long *s = (long long *)malloc(sizeof(long long)*m);
-    long long *sum = (long long *)malloc(sizeof(long long)*m);
+
     for(int i = 0;i<m;i++){
         s[i] = 0;
-        sum[i] = 0;
     }
     for(int i = 0;i<n;i++){
         while(k>-1 && copy[k+1] != source[i]){
@@ -21,11 +19,6 @@ long long *KMP(char *source, char *copy, long long *arr){
         if(copy[k+1]== source[i]){
             k++;
             s[k]++;
-
-            /*long long o = arr[k];
-            if(o!= -1){
-                sum[o]++;
-            }*/
         }
         if(k+1== m){
             k = arr[k];
@@ -35,22 +28,20 @@ long long *KMP(char *source, char *copy, long long *arr){
         if(arr[i] != -1){
             s[arr[i]] = s[arr[i]] + s[i];
         }
-
+        s[i] = s[i]*pla[i];
     }
     return s;
 }
 
+long long *Prefix(char* str, long long *arr, long long size){
 
-long long *Prefix(char* str, long long *arr){
-    long long size = strlen(str);
     long long *pla = (long long *)malloc(sizeof(long long)*size);
-
     for(int i = 0;i<size;i++){
         arr[i] = -1;
         pla[i] = 1;
     }
     long long k = -1;
-    for(int i = 1;i<size;i++){//RE
+    for(int i = 1;i<size;i++){
         while(k>-1 && str[k+1] != str[i])
             k = arr[k];
         if(str[k+1] == str[i]){
@@ -73,11 +64,11 @@ int main(){
     
     long long m = strlen(copy);
     long long *arr = (long long *)malloc(sizeof(long long)*m);
-    long long *pla = Prefix(copy,arr);
-    long long *s = KMP(source,copy,arr);
+    long long *pla = Prefix(copy,arr,m);
+    long long *s = KMP(source,copy,arr,m,pla);
 
     for(int i = 0;i<m;i++){
-        printf("%lld",s[i]*pla[i]);
+        printf("%lld",s[i]);
         printf("\n");
     }
 }
